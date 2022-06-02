e905c71537b320d55b960d2d6f8990db96dead2a
Zeth Lietzau <z@next-iteration.net>
2022-06-02 08:51:58 -0600

    added matching max height to cards

--- js/global.js
+++ js/global.js
@@ -252,3 +252,28 @@ slideUps.forEach(function (slideUp) {
 });
 
 /* END ANIMATIONS */
+
+if ((cardsets = document.querySelectorAll(".kivvi-cardset"))) {
+    cardsets.forEach((cardset) => {
+        window.addEventListener("load", function () {
+            let cardsetItems = cardset.querySelectorAll(".kivvi-card");
+            minimgheight = false;
+            cardsetItems.forEach((item) => {
+                let img = item.querySelector(".imgwrapper img");
+
+                if (
+                    img.getBoundingClientRect().height > 0 &&
+                    (!minimgheight ||
+                        img.getBoundingClientRect().height < minimgheight)
+                ) {
+                    minimgheight = img.getBoundingClientRect().height;
+                }
+            });
+            cardsetItems.forEach((item) => {
+                let img = item.querySelector(".imgwrapper img");
+                console.log(img);
+                img.style.maxHeight = minimgheight + "px";
+            });
+        });
+    });
+}
